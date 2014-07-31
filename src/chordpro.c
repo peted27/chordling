c477fd09be9ab61450a8c89df94e6a8509a4916e
peted <peted27@gmail.com>
2014-07-31 11:52:07 +1000

    bit messy

--- src/chordpro.c
+++ src/chordpro.c
@@ -9,17 +9,35 @@ void terminate_buffer(char *buffer);
 
 BOOLEAN process_file(char *filename)
 {
+	/* file parsing */
 	FILE *fp;
-	char buffer[BUFFER_LENGTH];
-	char chord_buffer[BUFFER_LENGTH];
-	char lyric_buffer[BUFFER_LENGTH];
-
+	char c;
 	BOOLEAN parsing_status = TRUE;
 	int line_count = 0;
 
+	/* temporary buffers */
+	char lin_buffer[BUFFER_LENGTH];
+	char chd_buffer[BUFFER_LENGTH];
+	char lyr_buffer[BUFFER_LENGTH];
+	char dir_buffer[BUFFER_LENGTH];
+	char arg_buffer[BUFFER_LENGTH];
+
+	/* buffer indexes */
+	int lin_buf_index = 0;
+	int chd_buf_index = 0;
+	int lyr_buf_index = 0;
+	int dir_buf_index = 0;
+	int arg_buf_index = 0;
+
+	/* in section bools */
+	BOOLEAN in_chord = FALSE;
+	BOOLEAN in_tab = FALSE;
+	BOOLEAN in_comment = FALSE;
+	BOOLEAN in_directive = FALSE;
+	BOOLEAN in_directive_arg = FALSE;
+
+
 	TRACE("Opening file: %s\n", filename);
-	
-	/* */
 	fp = fopen(filename, "r");
 
 	/* file couldnt be opened */
@@ -30,29 +48,95 @@ BOOLEAN process_file(char *filename)
 	}
 	
 	/* loop over all lines in the file */
-	while (fgets(buffer, BUFFER_LENGTH, fp) != NULL && parsing_status)
+	while ((c = fgetc(fp)) != EOF && parsing_status)
 	{
-		line_count++;
 
-		TRACE("line: %d .. ", line_count);
-		
-		terminate_buffer(buffer);
-
-		TRACE("%s\n", buffer);
-
-		chord_buffer[0] = '\0';
-		lyric_buffer[0] = '\0';
-		
-		/* do something with line */
-		switch(buffer[0])
+		switch (c)
 		{
-		case '#':
-			break;
 		case '{':
+			if (in_directive)
+			{
+				fprintf(stderr, "Error, in %s at line %d. Already in directive\n", filename, line_count);
+			}
+			else
+			{
+				in_directive = TRUE;
+			}
+			break;
+
+
+		case '}':
+			in_directive = FALSE;
+			in_directive_arg = FALSE;
+			/* process directive */
+			break;
+
+		case ':':
+			if (in_directive)
+			{
+				in_directive_arg = TRUE;
+			}
+			else
+			{
+				
+			}
+			break;
+
+
+		case '[':
+			if (in_chord)
+			{
+				fprintf(stderr, "Error, in %s at line %d. Already in chord\n", filename, line_count);
+			}
+			else
+			{
+				in_chord = TRUE;
+				chd_buf_index = 0;
+			}
+			break;
+
+
+		case ']':
+			if (!in_chord)
+			{
+			}
+			else
+			{
+				in_chord = FALSE;
+			}
+			/* process chord */
+			break;
+
+
+		case '#':
+			in_comment = TRUE;
+			break;
+
+
+		case '\n':
+		case '\r':
+			line_count++;
+			/* process line */
 			break;
 		default:
-			break;
+			if (in_chord)
+			{
+				chd_buffer[chd_buf_index++] = c;
+			}
+			else if (in_directive_arg)
+			{
+			}
+			else if (in_directive)
+			{
+			}
+			else if (in_tab)
+			{
+			}
+			else
+			{
+			}
 
+			break;
 		}
 
 		
